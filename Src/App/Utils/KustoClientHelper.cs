d2c35c35b19be04e2552536d29d13d2117ef7180
Yochai Gilad <yogilad@microsoft.com>
2023-09-03 22:42:27 +0300

    Uploading file to stream almost works
    Writes a namespace into the zip file instead of the content of the file

--- Src/App/Utils/KustoClientHelper.cs
+++ Src/App/Utils/KustoClientHelper.cs
@@ -1,21 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Compression;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+
+using Azure.Storage.Blobs;
+using Kusto.Data;
+using Kusto.Data.Common;
+using Kusto.Data.Net;
+using Kusto.Data.Net.Client;
+using Kusto.Ingest;
 
 namespace Klipboard.Utils
 {
     public class KustoClientHelper
     {
-        private string m_connectionString = string.Empty;
+        private readonly KustoConnectionStringBuilder m_engineKcsb;
+        private readonly KustoConnectionStringBuilder m_dmKcsb;
         private string m_databaseName = string.Empty;
 
-        public KustoClientHelper() { }
-
-        public bool TryUploadFileToEngineStagingArea(string filePath, out string blobUri) 
+        public KustoClientHelper(KustoConnectionStringBuilder connectionString, string databaseName)
         {
-            throw new NotImplementedException();
+            m_engineKcsb = new KustoConnectionStringBuilder(connectionString);
+            m_dmKcsb = new KustoConnectionStringBuilder(connectionString);
+            m_databaseName = databaseName;
+
+            m_dmKcsb.DataSource = "ingest-" + m_dmKcsb.DataSource;
+        }
+
+        public bool TryUploadFileToEngineStagingArea(string filePath, string upstreamFileName, out string? blobUri, out string? error) 
+        {
+            var engineClient = KustoClientFactory.CreateCslAdminProvider(m_engineKcsb);
+
+            error = null;
+            blobUri = null;
+
+            try 
+            {
+                var res = engineClient.ExecuteControlCommand(m_databaseName, ".create tempstorage");
+                res.Read();
+                
+                var tempStorage = res.GetString(0);
+                if (TryUploadFromFile(tempStorage, filePath, upstreamFileName, out blobUri, out error))
+                {
+                    return true;
+                }
+
+                return false;
+            }
+            catch (Exception ex) 
+            { 
+                error = "Failed to get engine staging account: " + ex.Message;
+                return false;
+            }
         }
 
         public bool TryUploadFileToDataManagementStagingArea(string filePath, out string blobUri)
@@ -45,6 +84,66 @@ namespace Klipboard.Utils
         public bool TryQueueIngestFile(string tableName, string fileName, object fileSourceOptions, object ingestionProperties)
         {
             throw new NotImplementedException();
+        }
+
+        private static bool TryUploadFromFile(string blobContainerUriStr, string localFilePath, string upstreamFileName, out string? blobUri, out string? error)
+        {
+            if (!TryCreateZipStream(localFilePath, upstreamFileName, out var memoryStream, out error))
+            {
+                blobUri = null;
+                return false;
+            }
+
+            upstreamFileName += ".zip";
+            
+            using (memoryStream)
+            {
+                var blobContainerUri = new Uri(blobContainerUriStr);
+                var containerClient = new BlobContainerClient(blobContainerUri);
+                var blobClient = containerClient.GetBlobClient(upstreamFileName);
+                var res = blobClient.Upload(memoryStream, false).GetRawResponse();
+
+                if (res.IsError)
+                {
+                    blobUri = null;
+                    error = res.ReasonPhrase;
+                    return false;
+                }
+
+                blobUri = blobContainerUriStr.Replace("?", $"/{upstreamFileName}?");
+                error = null;
+                return true;
+            }
+        }
+
+        private static bool TryCreateZipStream(string filePath, string upsteramFileName, out MemoryStream memoryStream, out string? error)
+        {
+            try
+            {
+                memoryStream = new MemoryStream();
+
+                using (var archive = new ZipArchive(memoryStream, ZipArchiveMode.Create, true))
+                {
+                    var zipFile = archive.CreateEntry(upsteramFileName);
+
+                    using (var entryStream = zipFile.Open())
+                    using (var streamWriter = new StreamWriter(entryStream))
+                    using (var fileStream = new FileStream(filePath, FileMode.Open))
+                    {
+                        streamWriter.Write(fileStream);
+                    }
+                }
+
+                memoryStream.Seek(0, SeekOrigin.Begin);
+                error = null;
+                return true;
+            }
+            catch (Exception ex) 
+            {
+                error = ex.Message;
+                memoryStream = null;
+                return false;
+            }
         }
     }
 }
---
+++ Src/Test/UtilsTest/KustoClientHelperTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Klipboard.Utils;
+using Kusto.Cloud.Platform.Utils;
+using Kusto.Data;
+
+namespace Klipboard.Utils.Test
+{
+    [TestClass]
+    public class KustoClientHelperTests
+    {
+        private KustoClientHelper m_kustoHelper;
+
+        public KustoClientHelperTests()
+        {
+            var appConfig = AppConfigFile.CreateDebugConfig().ConfigureAwait(false).ResultEx();
+            var kcsb = new KustoConnectionStringBuilder(appConfig.DefaultClusterConnectionString).WithAadUserPromptAuthentication();
+            m_kustoHelper = new KustoClientHelper(kcsb, appConfig.DefaultClusterDatabaseName);
+        }
+
+        [TestMethod]
+        public void GivenFile_WhenUploadToEngineStore_BlobIsCreated()
+        {
+            var dt = DateTime.Now;
+            var upsteramFileName = $"TestFile_{dt.Year}{dt.Month}{dt.Day}_{dt.Hour}{dt.Minute}{dt.Second}_{Guid.NewGuid().ToString()}"; 
+            var res = m_kustoHelper.TryUploadFileToEngineStagingArea("C:\\Users\\yocha\\Desktop\\Klipboard Test Data\\snp.csv", upsteramFileName, out var blobUri, out var errorMsg);
+
+            Assert.IsTrue(res);
+            Assert.IsFalse(string.IsNullOrWhiteSpace(blobUri));
+            Assert.IsTrue(string.IsNullOrWhiteSpace(errorMsg));
+        }
+    }
+}
