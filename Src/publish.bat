4d70a9c4d03668c91fd5a0ffab17e6d1323f376f
Yochai Gilad <yochaig@gmail.com>
2023-09-29 15:46:12 +0300

    Build script works well

--- Src/publish.bat
+++ Src/publish.bat
@@ -1,7 +1,7 @@
 REM * Where is MSBUILD 
 set MSBUILD=msbuild.exe
 where msbuild.exe 
-if %errorlevel% == 1 ( set MSBUILD="C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" )
+if %errorlevel% equ 1 ( set MSBUILD="C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" )
 
 REM * Clean
 rmdir App\Klipboard\bin\Release /s /q
@@ -9,12 +9,31 @@ rmdir App\Klipboard\bin\Publish /s /q
 
 REM * Build 
 %MSBUILD% -p:configuration=Release -t:restore
+if %errorlevel% neq 0 ( goto BuildFailed)
+
 %MSBUILD% -p:configuration=Release -t:build
+if %errorlevel% neq 0 ( goto BuildFailed)
 
 REM * Publish
-%MSBUILD% /t:publish /p:PublishProfile=ClickOnce -p:PublishDir="bin\Publish\ClickOnce"
-%MSBUILD% /t:publish /p:PublishProfile=SingleFile -p:PublishDir="bin\Publish\SingleFile"
+%MSBUILD% /t:publish /p:PublishProfile=ClickOnce -p:PublishDir="bin\Publish\Klipboard_ClickOnce"
+if %errorlevel% neq 0 ( goto BuildFailed)
+
+%MSBUILD% /t:publish /p:PublishProfile=SingleFile -p:PublishDir="bin\Publish\Klipboard"
+if %errorlevel% neq 0 ( goto BuildFailed)
 
 REM * Zip results
-tar -acf App\Klipboard\bin\Publish\Klipboard.zip ".\App\Klipboard\bin\Publish\SingleFile\Klipboard.pdb" ".\App\Klipboard\bin\Publish\SingleFile\Klipboard.exe"
-tar -acf App\Klipboard\bin\Publish\Klipboard_ClickOnce.zip ".\App\Klipboard\bin\Publish\ClickOnce"
+cd App\Klipboard\bin\Publish\
+tar -acf Klipboard.zip "Klipboard\Klipboard.pdb" "Klipboard\Klipboard.exe"
+if %errorlevel% neq 0 ( goto BuildFailed)
+
+tar -acf Klipboard_ClickOnce.zip "Klipboard_ClickOnce"
+if %errorlevel% neq 0 ( goto BuildFailed)
+
+echo Build Succeeded!
+goto End
+
+:BuildFailed
+echo Build Failed!
+
+:End
+cd ..\..\..\..
