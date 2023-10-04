88fa9eff89d064d3d20e186158121bafe7d8c347
Yochai Gilad <yogilad@microsoft.com>
2023-10-05 01:22:22 +0300

    Add tutorial sample data files (#64)
    * Correct wiki home page
    
    * Updated readme links
    
    * wiki wiki
    
    * data samples
    
    * SNP and HepV sample data
    
    * samples for later E2Es
    
    * external data samples
    
    * Add Temp Table Sample

--- Src/App/Workers/Misc/HelpWorker.cs
+++ Src/App/Workers/Misc/HelpWorker.cs
@@ -31,11 +31,11 @@ namespace Klipboard.Workers
             switch(chosenOption)
             {
                 case Help:
-                    OpSysHelper.InvokeLink("https://github.com/yogilad/Klipboard/wiki/Welcome-to-Klipboard");
+                    OpSysHelper.InvokeLink("https://github.com/yogilad/Klipboard/wiki");
                     break;
 
                 case About:
-                    var msg = $"Version '{AppConstants.ApplicationVersion}'\nDeveloped by Yochai Gilad.\nhttps://github.com/yogilad/Klipboard/";
+                    var msg = $"Version '{AppConstants.ApplicationVersion}'\nDeveloped by Yochai Gilad.\nhttps://github.com/yogilad/Klipboard/Wiki";
                     sendNotification(AppConstants.ApplicationName, msg);
                     break;
 
@@ -82,7 +82,7 @@ namespace Klipboard.Workers
 @"Hi, 
 
 I'm using Klipboard for Kusto, and I think you'd find it useful. 
-You can get it from https://github.com/yogilad/Klipboard/";
+https://github.com/yogilad/Klipboard/Wiki";
 
             var link = $"mailto:?subject={subject}&body={Uri.EscapeUriString(body)}";
 
--- ThirdPartyAtribution.md
+++ ThirdPartyAtribution.md
@@ -10,3 +10,9 @@
 * [Letter-k icons](https://www.flaticon.com/free-icons/letter-k) created by Md Tanvirul Haque - Flaticon
 * [Magic wand](https://www.flaticon.com/free-icon/wand_1626598) icons created by deemakdaksina - Flaticon
 * [Download file icons](https://www.flaticon.com/free-icon/apps_9147708) created by Radhe Icon - Flaticon
+
+## Sample Data
+* [Kusto Help Cluster](https://help.kusto.windows.net)
+* [LogApi/LogHub on Github](https://github.com/logpai/loghub)
+* [TLC Trip Record Data on NYC.Gov](https://www.nyc.gov/site/tlc/about/tlc-trip-record-data.page)
+* [HepC Immunization Data on WHO.Int](https://apps.who.int/gho/data/node.main.A824?lang=en)
