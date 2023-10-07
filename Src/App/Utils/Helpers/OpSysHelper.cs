0692cc0bb094636a04b77235899e764d09ca7169
Yochai Gilad <yochaig@gmail.com>
2023-10-07 22:21:35 +0300

    Single process lock

--- Src/App/Utils/Helpers/OpSysHelper.cs
+++ Src/App/Utils/Helpers/OpSysHelper.cs
@@ -3,6 +3,22 @@
 
 namespace Klipboard.Utils
 {
+    public class AutoDispose : IDisposable
+    {
+        Action m_disposeAction;
+        
+        public AutoDispose(Action disposeAction)
+        {
+            m_disposeAction = disposeAction;
+        }
+
+        public void Dispose() 
+        {
+            m_disposeAction?.Invoke();
+            m_disposeAction = null;
+        }
+    }
+
     public static class OpSysHelper
     {
         public static void InvokeLink(string link)
@@ -14,6 +30,26 @@ namespace Klipboard.Utils
             });
 
             return;
+        }
+
+        public static bool TryAcquireSingleProcessLock(out AutoDispose? processLock)
+        {
+#if DEBUG
+            var name = $"{AppConstants.ApplicationName}_DEBUG";
+#else
+            var name = $"{AppConstants.ApplicationName}_RELEASE";
+#endif
+            var singleProcessMutex = new Mutex(initiallyOwned: false, name);
+
+            if (!singleProcessMutex.WaitOne(0))
+            {
+                singleProcessMutex.Dispose();
+                processLock = null;
+                return false;
+            }
+
+            processLock = new AutoDispose(() => singleProcessMutex.ReleaseMutex());
+            return true;
         }
     }
 }
